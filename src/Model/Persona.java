df2decc13951f4267e9c6cb64ad4ca7dbc6bbc83
Rodrigo Gutierrez <rodrigo.gutierrezb@usm.cl>
2023-06-15 09:50:16 -0400

    Se removieron las clases de persistencia.

---
+++ src/Model/Persona.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.LinkedList;
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author RGutierrezB
+ */
+@Entity
+public class Persona implements Serializable {
+    
+    //atributos básicos de una persona
+    @Id
+    private int rut;
+    
+    @Basic
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    
+    @Temporal(TemporalType.DATE)
+    private Date fechaNacimiento;
+    
+    //@OneToMany(mappedBy="dueno")
+    //private LinkedList<Mascota> mascotas;
+
+    public Persona() {
+    }
+
+    public Persona(int rut, String nombre, String apellidoPaterno, String apellidoMaterno, Date fechaNacimiento) {
+        this.rut = rut;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+//    public LinkedList<Mascota> getMascotas() {
+//        return mascotas;
+//    }
+
+//    public void setMascotas(LinkedList<Mascota> mascotas) {
+//        this.mascotas = mascotas;
+//    }
+    
+
+    @Override
+    public String toString() {
+        return "Persona{" + "rut=" + rut + ", nombre=" + nombre + ", apellidoPaterno=" + apellidoPaterno + ", apellidoMaterno=" + apellidoMaterno + ", fechaNacimiento=" + fechaNacimiento + '}';
+    }
+    
+    
+}
---
+++ src/Model/Veterinaria.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author RGutierrezB
+ */
+public class Veterinaria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+    }
+    
+}
